5ebef5e5bd04a69d3f32d364ee4ea65759efa04f
Ihor Sliunko <ihor.sliunko.dev@gmail.com>
2025-02-08 19:31:58 +0200

    Project Setup

---
+++ src/main/java/ua/iate/itblog/ItBlogApplication.java
@@ -0,0 +1,13 @@
+package ua.iate.itblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/iate/itblog/ItBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.iate.itblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
