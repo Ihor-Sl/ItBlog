ae35f316413dcde0e43d4a782c759179116deebf
shagid76 <igrish76@gmail.com>
2025-02-08 12:45:32 -0500

    BLOG-1 test commit

--- src/main/java/ua/iate/itblog/ItBlogApplication.java
+++ src/main/java/ua/iate/itblog/ItBlogApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ItBlogApplication {
 
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(ItBlogApplication.class, args);
 	}
